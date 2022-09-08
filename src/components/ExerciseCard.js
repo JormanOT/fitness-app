ccc4db952a70e368a544ed193682fc4a29058164
Jorman Ortega <jormanortega34@gmail.com>
2022-09-08 14:52:00 -0400

    Añadido paginacion y busqueda dinamica por categoria

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import { Button , Stack , Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom'
 
-const ExerciseCard = ({exercise}) => {
+const ExerciseCard = ({ exercise }) => {
     return (
         <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
             <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+            <Stack direction="row">
+                <Button sx={{
+                    ml: '21px',
+                    color: '#fff',
+                    backgroundColor: '#ffa9a9',
+                    fontSize: '14px',
+                    borderRadius: '20px',
+                    textTransform: 'capitalize'
+                }}>
+                    {exercise.bodyPart}
+                </Button>
+                <Button sx={{
+                    ml: '21px',
+                    color: '#fff',
+                    backgroundColor: '#fcc757',
+                    fontSize: '14px',
+                    borderRadius: '20px',
+                    textTransform: 'capitalize'
+                }}>
+                    {exercise.target}
+                </Button>
+            </Stack>
+                <Typography ml='21px' mt='11px' pb='10px' fontWeight='bold' textTransform='capitalize' color='#000' fontSize='22px'>
+                    {exercise.name}
+                </Typography>
         </Link>
     )
 }
