f20ea640532292b02bae9301d6ebb3051de1efba
JormanOT <jormanortega34@gmail.com>
2022-11-10 21:44:09 +0100

    Feature : Añadido Footer a la pagina

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgColr="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="Logo" width="200px" heigth="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Made with ❤️ By Jorman Ortega
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
